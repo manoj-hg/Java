12a218f22d64a42cfd717903f49ae61f06482f00
Manoj H.G <manojhg321@gmail.com>
2026-02-11 19:19:01 +0530

    Add files via upload

---
+++ Int.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Int {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the integer:");
+        int i=sc.nextInt();
+        System.out.println("The entered integer is:"+i);
+    }
+}
