c0a513a6c1fce625469359b879a1f4d7ba68557a
Manoj H.G <manojhg321@gmail.com>
2026-02-28 21:39:24 +0530

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,7 @@
+public class Alpha {
+        public static void main(String[]args) {
+            char c;
+            for(c = 'A'; c <= 'Z'; ++c)
+                System.out.print(c + " ");
+        }
+    }
